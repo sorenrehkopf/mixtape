113679dbe8d9f594a658b000fe9e8eae0c62b5e9
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2018-05-22 00:18:53 -0700

    Client moved to own package

--- src/components/create-playlist/actions/clear-created-playlist.js
+++
@@ -1,9 +0,0 @@
-import {
-	CLEAR_CREATED_PLAYLIST
-} from './types';
-
-const clearCreatedPlaylist = () => (dispatch, getState) => {
-	dispatch({ type: CLEAR_CREATED_PLAYLIST });
-}
-
-export default clearCreatedPlaylist;
